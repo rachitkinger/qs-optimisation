a82ceb3bb6c40eb9c202b8751e8ddf6d32c77658
Rachit Kinger <Rachit.Kinger@jpress.co.uk>
2017-09-12 11:09:40 +0100

    app deployed on shinyapps.io

--- qs-optimiser/ui.R
+++ qs-optimiser/ui.R
@@ -4,10 +4,19 @@ library(shiny)
 ui <- fluidPage(
     
     # App title ----
-    titlePanel("QS Optimisation (beta)"),
+    titlePanel("QS Optimisation - beta"),
     
     # Sidebar layout with input and output definitions ----
     fluidRow(
+        column(4,
+               p("The project is currently in beta, so we apologise in advance for any unexpected
+                 behaviour that you experience. If you come across any bugs please ",
+                 a(href="mailto:rachit.kinger@jpress.co.uk", "email us.")),
+               p("For usage documentation please visit ", 
+                 a(href="https://rachitkinger.github.io/qs-optimisation/", "the project page "),
+                 "or visit our open source project repository on ", 
+                 a(href="https://github.com/rachitkinger/qs-optimisation", "github."))
+        ),
         column(4,
                fileInput(inputId ="file1", 
                          label = "Choose CSV File",
@@ -32,18 +41,6 @@ ui <- fluidPage(
     ),
     
     fluidRow(
-        
-        h2("Priority 1 Campaigns"),
-        tableOutput(outputId = "p1.table")
-    ),
-    
-    fluidRow(
-        h2("Priority 2 Campaigns"),
-        tableOutput(outputId = "p2.table")
-    ),
-    
-    
-    fluidRow(
         column(8),
         
         column(4,
@@ -53,77 +50,16 @@ ui <- fluidPage(
     ),
     
     fluidRow(
-        DT::dataTableOutput(outputId = "final_table")
+        tableOutput(outputId = "p1.table")
+    ),
+    
+    fluidRow(
+        tableOutput(outputId = "p2.table")
+    ),
+    
+    
+    fluidRow(
+        DT::dataTableOutput(outputId = "final.table")
     )
 )
-   
-#     
-#     sidebarLayout(
-#         
-#         # Sidebar panel for inputs ----
-#         sidebarPanel(
-#             
-#             # Input: Select a file ----
-#             fileInput(inputId ="file1", 
-#                       label = "Choose CSV File",
-#                       multiple = FALSE,
-#                       accept = c("text/csv",
-#                                  "text/comma-separated-values,text/plain",
-#                                  ".csv")),
-#             
-#             # Horizontal line ----
-#             tags$hr(),
-#             
-#             
-#         # Input: slider for budget range
-#             sliderInput(inputId = "budget.range",
-#                         label = "Select 30-Day Spend Range",
-#                         min = 100,
-#                         max = 5000,
-#                         value = c(300,2000),
-#                         step = 50,
-#                         round = TRUE,
-#                         ticks = TRUE,
-#                         pre = "£",
-#                         post =".00"
-#                         ),
-#         
-#         h3("Upload .csv files only")
-#         ),
-#         
-#         # Main panel for displaying outputs ----
-#         mainPanel(
-#             
-#             
-#                     # Output: Data file ----
-#                     h2("Priority 1 Campaigns"),
-#                     tableOutput(outputId = "p1.table"),
-#                     br(),
-#                     h2("Priority 2 Campaigns"),
-#                     tableOutput(outputId = "p2.table"),
-#                     
-#                     # fluidRow(
-#                     #     column(2,
-#                     #            selectInput(inputId = "priority",
-#                     #                        label = "Priority Level:",
-#                     #                        c("All", "Priority 1", "Priority 2"),
-#                     #                        multiple = TRUE)
-#                     #     ),
-#                     #     column(2,
-#                     #            selectInput(inputId = "campaign",
-#                     #                        label = "Campaign:",
-#                     #                        c("All")
-#                     #                        multiple = TRUE)
-#                     #     )
-#                     # ),
-#                     # 
-#                     # Create a new row for the table.
-#                     
-#                     fluidRow(
-#                         DT::dataTableOutput(outputId = "final.table")
-#                     )
-#            
-#         )
-#         
-#     )
-# )
+ 
